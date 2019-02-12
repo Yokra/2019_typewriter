4301c32331705339d6be43875dff92d4b20cd5e7
yokra <yordanka_krasteva@mail.bg>
2019-02-12 09:10:11 +0100

    last commit

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,22 @@
 "use strict";
+let typewriterText = document.querySelector("#typewriter").textContent;
+let counter = 0;
+
+init();
+
+function init() {
+  document.querySelector("#typewriter").textContent = "";
+
+  typewriterLoop();
+}
+
+function typewriterLoop() {
+  counter++;
+  document.querySelector("#typewriter").textContent = typewriterText.substring(
+    0,
+    counter
+  );
+  if (typewriterText.length > counter) {
+    setTimeout(typewriterLoop, Math.random() * 200);
+  }
+}
